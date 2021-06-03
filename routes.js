b5bb0e5b39154571cc6db4536e2b92be1529f22e
JoaoGSP <63313287+JoaoGSP@users.noreply.github.com>
2021-06-03 15:00:23 -0300

    Autenticação
    Autorização do sistema

--- routes.js
+++ routes.js
@@ -62,7 +62,7 @@ routes.delete("/attendants/:id", AttendantController.delete)
 
 //Cadastro e controle de consultas
 
-//module.exports = routes;
+
 export default routes;
 
 
@@ -72,30 +72,6 @@ export default routes;
 //Incluir o middleware authMiddleware nas rotas de produtos, para garantir que essas//
 //operações sejam realizadas apenas a usuários autenticados.//
 
-
-// GET /products > Listar produtos
-// /products?page=1&limit=10
-routes.get("/products", ProductController.list);
-// GET /products/:id > Listar um produto
-routes.get("/products/:id", ProductController.listOne);
-// POST /products > Criar um produto
-routes.post("/products", ProductController.create);
-// PUT /products/:id > Atualizar um produto
-routes.put("/products/:id", ProductController.update);
-// DELETE /products/:id > Deletar um produto
-routes.delete("/products/:id", ProductController.delete); 
-
 //Login//
-//Criar a rota de login com o método post.
 
-//User//
-routes.post("/users", UserController.create);
-
-//Se a autenticação estiver funcionando adequadamente, pode utilizar essas rotas como teste.//
-
-
-// Me (usuário autenticado)//
-routes.get("/me", authMiddleware, UserController.listMe);
-routes.put("/me", authMiddleware, UserController.updateMe);
-routes.delete("/me", authMiddleware, UserController.deleteMe);
 */
